2ee4555d6da0b6f1c2f55276b44f9b93ae5e20d8
sychx <37225059+sychx@users.noreply.github.com>
2024-10-02 18:00:56 -0300

    feat: translate english

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
-#include <winuser.h>
+#include <windows.h>
 
 int display_message_error() {
-  int msgbox =
-      MessageBox(NULL,
-                 (LPCWSTR)L"O hardware do seu computador não é compatível com este software.\nDeseja tentar novamente ?",
-                 (LPCWSTR)L"Windows Error",
-                 MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2);
+    int msgbox = MessageBox(NULL,
+    "Your computer's hardware is not compatible with this software.\nWould you like to try again?",
+    "Windows Error",
+    MB_YESNO | MB_OKCANCEL | MB_ICONERROR);
 }
 
 int main(void) {
-  int msgbox_id;
+    int msgbox_id;
 
-  while(true) {
-    msgbox_id = display_message_error();
+    while(true) {
+        msgbox_id = display_message_error();
 
-    switch (msgbox_id) {
-    case IDCANCEL:
-      exit(0);
-    case IDTRYAGAIN:
-    case IDCONTINUE:
-      exit(0);
+        switch (msgbox_id) {
+        case IDCANCEL:
+            exit(0);
+        case IDTRYAGAIN:
+        case IDCONTINUE:
+            exit(0);
+        }
     }
-  }
-
-  return 0;
+    return 0;
 }
